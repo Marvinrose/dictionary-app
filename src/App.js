e609caba8cc10bd6b53e471c83407b5e49d267ce
Marvinrose <rozzeymarvin32@gmail.com>
2022-09-28 04:58:36 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="container">
+      <h1>MARVIN DICTIONARY</h1> <br />
       <Dictionary />
     </div>
   );
