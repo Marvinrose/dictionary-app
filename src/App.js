335efeecc06f300f619463f3584c8f3c1f582d1c
Marvinrose <rozzeymarvin32@gmail.com>
2022-09-30 23:44:34 +0100

    Added the dictionary image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import dictionary from "./dictionary-gif.gif";
 import "bootstrap/dist/css/bootstrap.css";
 import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="container">
-      <h1>MARVIN DICTIONARY</h1> <br />
+      <img
+        src={dictionary}
+        alt="logo"
+        className="img-fluid  mx-auto d-block"
+        width={200}
+      ></img>
+      <h1 className="text-center">MARVIN DICTIONARY</h1> <br />
       <Dictionary defaultKeyword="rose" />
     </div>
   );
