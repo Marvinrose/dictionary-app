ec800d49dbf6e8f036df09b2b1d9852952aefcc9
Marvinrose <rozzeymarvin32@gmail.com>
2022-10-04 14:56:54 +0100

    Edited the react-loader animation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -67,15 +67,17 @@ export default function Dictionary(props) {
   } else {
     load();
     return (
-      <ColorRing
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="blocks-loading"
-        wrapperStyle={{}}
-        wrapperClass="blocks-wrapper"
-        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
-      />
+      <div className="loader">
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+          colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+        />
+      </div>
     );
   }
 }
